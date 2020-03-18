af08aab446d359d7599c22ca87d7cf9ece2eea59
Nicknaaaame <48119336+Nicknaaaame@users.noreply.github.com>
2020-03-18 14:45:41 +0700

    Add files via upload

---
+++ Lab22Extention/IntExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab22Extension {
+    public static class IntExtension {
+        public static bool IsPalindrome(this int value) {
+            string num = value.ToString();
+            int lenght = num.Length;
+
+            for(int i = 0; i< lenght / 2; ++i)
+                if (!num[i].Equals(num[lenght - i - 1]))
+                    return false;
+
+            return true;
+        }
+    }
+}

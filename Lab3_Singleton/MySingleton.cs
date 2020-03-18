5cde4ad2984460a06e7d29148050f5a29ba69fad
Nicknaaaame <48119336+Nicknaaaame@users.noreply.github.com>
2020-03-18 14:44:51 +0700

    Add files via upload

---
+++ Lab3_Singleton/MySingleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5Singleton {
+    class MySingleton {
+        private MySingleton() {
+        }
+
+        public static MySingleton GetInstance() {
+            return MySingletonHolder.INSTANCE;
+        }
+
+        private class MySingletonHolder {
+            public static readonly MySingleton INSTANCE = new MySingleton();
+        }
+    }
+}
